7434b1475086647513026b444fbc1bcf6d0ec0cf
fuseter <phongkhanet.ka.61@ubu.ac.th>
2021-02-24 03:14:12 +0700

    fuse

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,15 +3,7 @@ import "./App.css";
 import HomePage from "./views/HomePage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <div>sdsdsdsd</div>;
 }
 
 export default App;
